02e82a3c9fe0ce63fa9de5ac627a31ecc0be2930
wawan020501 <80882467+wawan020501@users.noreply.github.com>
2021-03-18 16:10:12 +0800

    Add files via upload

---
+++ pr 2 udah jdi.py
@@ -0,0 +1,7 @@
+print("NAMA : riki purnama g.s \nNIM  : TI17200043 \nProdi: teknik informatiika (A)")
+
+def tinggi_gedung():
+    print("\ntinggi = 100m, naik = 5m, merosot = 4m\n")
+    print("\n jadi, untuk mencapai puncak membutuhkan",(5-4)*100),print("hari")
+
+tinggi_gedung()
